0050c75cbcad4418360c3c78dd0e0773fb284f47
Ramos <1joaoramos1@gmail.com>
2023-04-16 21:21:57 +0100

    Revert "Add files via upload"
    This reverts commit fdb451ce1fee7348cc9c8f0c664142f9896be4df.

--- app/lib/authentication/signUp.dart
+++ app/lib/authentication/signUp.dart
@@ -1,152 +1,273 @@
 import 'package:TraceBack/profile/profile.dart';
+import 'package:TraceBack/terms&guidelines/privacyAcceptance.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../util/editBox.dart';
 import '../posts/timeline.dart';
 import 'dart:ui';
-import 'login.dart';
 
 
-class signUpPage extends StatefulWidget {
+class SignUpPage extends StatefulWidget {
   @override
-  State<signUpPage> createState() => _signUpPageState();
+  State<SignUpPage> createState() => _SignUpPageState();
 }
 
-class _signUpPageState extends State<signUpPage>{
+class _SignUpPageState extends State<SignUpPage>{
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  bool _passwordVisible = false;
-  
- Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      leading: GoBackButton(),
-      backgroundColor: mainColor,
-      toolbarHeight: 80,
-    ),
-       body: LayoutBuilder(
-      builder: ( context, constraints) {
-        final double maxWidth = constraints.maxWidth;
-        final double fieldWidth = maxWidth * 0.8;
-        final double buttonWidth = maxWidth * 0.6;
-        final double minHeight = 50;
 
-        return SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child:Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0), 
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: constraints.maxHeight * 0.07),
-                  Container(
-                    margin: EdgeInsets.only(bottom:10),
-                    child: Align(
-                      alignment: Alignment.center,
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          backgroundColor: mainColor,
+          toolbarHeight: 80,
+        ),
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child: Center(
+                    child: Container(
                       child: Text(
                         'Sign Up',
                         style: TextStyle(
-                          fontSize: constraints.maxWidth * 0.07,
+                          fontSize: 24,
                           color: mainColor,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(
-                    width: fieldWidth,
-                    child: EditBox(text: "Name", hintText: "Name", isPassword: false)
-                    ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(
-                    width: fieldWidth,
-                    child: EditBox(text: "Email", hintText: "upXXXXXXXXX@up.pt", isPassword: false)
-                    ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(
-                    width: fieldWidth,
-                    child: EditBox(text: "Phone Number", hintText: "Phone Number", isPassword: false)
-                    ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(
-                    width: fieldWidth,
-                    child: EditBox(text: "Password", hintText: "Enter password", isPassword: true)
-                    ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(                   
-                    width: fieldWidth,
-                    child: EditBox(text: "Confirm Password", hintText: "Confirm password", isPassword: true)
-                    ),
+                ),
+                SizedBox(height: 16),
+                EditBox(text: "Name", hintText: "Name", isPassword: false),
+                EditBox(text: "Email", hintText: "upXXXXXXXXX@up.pt", isPassword: false),
+                EditBox(text: "Phone Number", hintText: "Phone Number", isPassword: false),
+                EditBox(text: "Password", hintText: "Enter password", isPassword: true),
+                EditBox(text: "Confirm Password", hintText: "Confirm password", isPassword: true),
 
-                  SizedBox(height: constraints.maxHeight * 0.05),
-                  Container(
-                    height: constraints.maxHeight * 0.08,
-                    margin: EdgeInsets.only(top: constraints.maxHeight * 0.025, bottom: constraints.maxHeight * 0.0125),
-                    width: constraints.maxWidth * 0.5,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => ProfilePage()
+                SizedBox(height: 15),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.only(top: 10, bottom: 5),
+                  width: 200,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.of(context)
+                            .push(
+                            MaterialPageRoute(
+                                builder: (context) => PrivacyAcceptancePage()
                             )
-                          );
-                        }
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(mainColor),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25.0),
-                          ),
-                        ),
-                      ),
-                      child: Text(
-                        "Register",
-                        style: TextStyle(
-                          fontSize: constraints.maxWidth * 0.045,
-                          fontWeight: FontWeight.bold,
+                        );
+                      }
+                      // função para guardar as informações
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(mainColor),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: constraints.maxHeight * 0.025),
-                  Container(
-  height: constraints.maxHeight * 0.03,
-  width: constraints.maxWidth * 0.75,
-  child: ElevatedButton(
-    onPressed: () {
-      Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
-    },
-    style: ButtonStyle(
-      backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-      elevation: MaterialStateProperty.all<double>(0),
-      side: MaterialStateProperty.all<BorderSide>(BorderSide.none),
-      overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
-    ),
-    child: FittedBox(
-      fit: BoxFit.scaleDown,
-      alignment: Alignment.center,
-      child: Text(
-        "Already have an account?",
-        style: TextStyle(
-          fontSize: constraints.maxWidth * 0.045,
-          fontWeight: FontWeight.bold,
-          color: mainColor,
-        ),
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 50),
+                Container(
+                  height: 25,
+                  width: 300,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .push(
+                          MaterialPageRoute(
+                              builder: (context) => ProfilePage()
+                          )
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      elevation: MaterialStateProperty.all<double>(0),
+                      side: MaterialStateProperty.all<BorderSide>(BorderSide.none),
+                      overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                    ),
+                    child: Text(
+                      "Already have an account? Login",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: mainColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
+class EditBox extends StatefulWidget {
+  final String text;
+  final String hintText;
+  final bool isPassword;
+  final TextEditingController? controller;
+
+  EditBox({required this.text, required this.hintText, this.isPassword = false, this.controller});
+
+  @override
+  _EditBoxState createState() => _EditBoxState();
+}
+
+class _EditBoxState extends State<EditBox> {
+  bool obscureText = true;
+  String? password;
+  String? confirmPassword;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 8),
+      padding: EdgeInsets.all(2),
+      width: 370,
+      child: TextFormField(
+        keyboardType: widget.text == 'Email'
+            ? TextInputType.emailAddress
+            : TextInputType.text,
+        obscureText: widget.isPassword ? obscureText : false,
+        decoration: InputDecoration(
+          label: Text(widget.text),
+          hintText: widget.hintText,
+          suffixIcon: widget.isPassword
+              ? GestureDetector(
+            onTap: () {
+              setState(() {
+                obscureText = !obscureText;
+              });
+            },
+            child: Icon(obscureText
+                ? Icons.visibility_off
+                : Icons.visibility),
+          )
+              : null,
+          filled: true,
+          fillColor: Colors.grey[300],
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide(
+              color: Colors.grey,
+              width: 2,
+              style: BorderStyle.solid,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide(
+              color: Colors.blue,
+              width: 1,
+              style: BorderStyle.solid,
+            ),
           ),
         ),
-          ),
-      
-        );
-      },
-    ),
-  );
+        onChanged: (value) {
+          if (widget.text == "Password") {
+            setState(() {
+              password = value;
+            });
+            if (confirmPassword != null && confirmPassword != value) {
+              setState(() {
+                confirmPassword = null;
+              });
+            }
+            password = value;
+          }
+        },
+        validator: widget.text == "Name"
+            ? (value) => nameValidator.validate(value)
+            : widget.text == "Email"
+            ? (value) => emailValidator.validate(value)
+            : widget.text == "Phone Number"
+            ? (value) => phoneValidator.validate(value)
+            : widget.text == "Password"
+            ? (value) => passwordValidator.validate(value)
+            : null,
+      ),
+    );
+  }
 }
+
+class nameValidator {
+  static String? validate(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a name';
+    }
+    return null;
+  }
+}
+
+class emailValidator {
+  static String? validate(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter an UP email address';
+    }
+
+    final upEmailRegex = RegExp(r'^up[0-9]+@up\.pt$');
+    if (!upEmailRegex.hasMatch(value)) {
+      return 'Please enter a valid UP email address';
+    }
+
+    return null;
+  }
+}
+
+class phoneValidator {
+  static String? validate(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a phone number';
+    }
+
+    final upEmailRegex = RegExp(r'^[0-9]*$');
+    if (!upEmailRegex.hasMatch(value)) {
+      return 'Please enter a valid phone number';
+    }
+    return null;
+  }
+}
+
+class passwordValidator {
+  static String? validate(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required';
+    }
+    if (value.length < 8) {
+      return 'Password must be at least 8 characters long';
+    }
+    if (!RegExp(r'[a-z]').hasMatch(value)) {
+      return 'Password must contain at least one lowercase letter';
+    }
+    if (!RegExp(r'[A-Z]').hasMatch(value)) {
+      return 'Password must contain at least one uppercase letter';
+    }
+    if (!RegExp(r'[0-9]').hasMatch(value)) {
+      return 'Password must contain at least one digit';
+    }
+    return null;
+  }
 }

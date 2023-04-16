775712cbe3cc2a9b5fcfc91482c0dd6c03fa11b4
DanielCarneiro123 <92723426+DanielCarneiro123@users.noreply.github.com>
2023-04-17 00:29:43 +0100

    Update login.dart

--- app/lib/authentication/login.dart
+++ app/lib/authentication/login.dart
@@ -16,6 +16,7 @@ class _LoginPageState extends State<LoginPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: Key("Login Page"),
       appBar: AppBar(
         backgroundColor: mainColor,
         toolbarHeight: 80,

20d313666e96d3739b5d6c402652c7a52821ae85
goncalobcosta <93865484+goncalobcosta@users.noreply.github.com>
2023-04-18 10:53:50 +0100

    Add files via upload

--- app/lib/authentication/login.dart
+++ app/lib/authentication/login.dart
@@ -1,124 +1,140 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'initial.dart';
 import '../posts/timeline.dart';
 import 'dart:ui';
+import 'dart:io';
 import 'signUp.dart';
 
 
 class LoginPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage>{
+class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: Key("Login Page"),
-      appBar: AppBar(
-        backgroundColor: mainColor,
-        toolbarHeight: 80,
-      ),
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 30),
-              Container(
-                margin: EdgeInsets.only(bottom:6),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Log In',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: mainColor,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              EditBox(text: "Email", hintText: "upXXXXXXXXX@up.pt"),
-              SizedBox(height: 10),
-              EditBox(text: "Password", hintText: "Password"),
-              SizedBox(height: 30),
-              Container(
-                height: 50,
-                margin: EdgeInsets.only(bottom: 50),
-                width: 200,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => SearchPage())
-                      );
-                    }
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(mainColor),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25.0),
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      leading: GoBackButton(),
+      backgroundColor: mainColor,
+      toolbarHeight: 80,
+    ),
+       body: LayoutBuilder(
+      builder: ( context, constraints) {
+        final double maxWidth = constraints.maxWidth;
+        final double fieldWidth = maxWidth * 1;
+        final double buttonWidth = maxWidth * 0.6;
+        final double minHeight = 50;
+
+       return SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child:Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: constraints.maxHeight * 0.07),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                          fontSize: constraints.maxWidth * 0.07,
+                          color: mainColor,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
-                  child: Text(
-                    "Save",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                  SizedBox(height: constraints.maxHeight * 0.09),
+                  Container(
+                    width: fieldWidth,
+                    child: EditBox(text: "Email",hintText: "upXXXXXXXXX@up.pt"),
+                  ),
+                  SizedBox(height: constraints.maxHeight * 0.0025),
+                  Container(
+                    width: fieldWidth,
+                    child: EditBox(
+                      text: "Password",
+                      hintText: "Password",
                     ),
                   ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                margin: EdgeInsets.only(top: 170),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Don't have an account? ",
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextButton(
+                  SizedBox(height: constraints.maxHeight * 0.05),
+                  Container(
+                    height: constraints.maxHeight * 0.08,
+                    margin: EdgeInsets.only(top: constraints.maxHeight * 0.025, bottom: constraints.maxHeight * 0.0125),
+                    width: constraints.maxWidth * 0.5,
+                    child: ElevatedButton(
                       onPressed: () {
-                        Navigator.of(context).push(
-                            MaterialPageRoute(builder: (context) => SignUpPage())
-                        );
+                        if (_formKey.currentState!.validate()) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => SearchPage()
+                            )
+                          );
+                        }
                       },
                       style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
-                        foregroundColor: MaterialStateProperty.all<Color>(mainColor),
-                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.zero),
-                        textStyle: MaterialStateProperty.all<TextStyle>(
-                          TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
+                        backgroundColor: MaterialStateProperty.all<Color>(mainColor),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
                           ),
                         ),
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        alignment: Alignment.centerLeft,
                       ),
-                      child: Text("SignUp"),
+                      child: Text(
+                        "Enter",
+                        style: TextStyle(
+                          fontSize: constraints.maxWidth * 0.045,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+                  ),
+                  SizedBox(height: constraints.maxHeight * 0.025),
+                  //Spacer(),
+                  Container(
+  height: constraints.maxHeight * 0.03,
+  width: constraints.maxWidth * 0.75,
+  child: ElevatedButton(
+    onPressed: () {
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) => signUpPage()));
+    },
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+      elevation: MaterialStateProperty.all<double>(0),
+      side: MaterialStateProperty.all<BorderSide>(BorderSide.none),
+      overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
+    ),
+    child: FittedBox(
+      fit: BoxFit.scaleDown,
+      alignment: Alignment.center,
+      child: Text(
+        "Don't have an account?",
+        style: TextStyle(
+          fontSize: constraints.maxWidth * 0.045,
+          fontWeight: FontWeight.bold,
+          color: mainColor,
         ),
       ),
-    );
-  }
+    ),
+  ),
+),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    ),
+  );
+}
 }
 
 class EditBox extends StatefulWidget {

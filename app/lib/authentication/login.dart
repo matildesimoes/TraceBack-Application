a3f6bbe78ddd74aa59ac9bf89d7e2a533702e787
DanielCarneiro123 <92723426+DanielCarneiro123@users.noreply.github.com>
2023-04-18 11:22:46 +0100

    Add files via upload

--- app/lib/authentication/login.dart
+++ app/lib/authentication/login.dart
@@ -16,125 +16,128 @@ class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      leading: GoBackButton(),
-      backgroundColor: mainColor,
-      toolbarHeight: 80,
-    ),
-       body: LayoutBuilder(
-      builder: ( context, constraints) {
-        final double maxWidth = constraints.maxWidth;
-        final double fieldWidth = maxWidth * 1;
-        final double buttonWidth = maxWidth * 0.6;
-        final double minHeight = 50;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        backgroundColor: mainColor,
+        toolbarHeight: 80,
+      ),
+      body: LayoutBuilder(
+        builder: ( context, constraints) {
+          final double maxWidth = constraints.maxWidth;
+          final double fieldWidth = maxWidth * 1;
+          final double buttonWidth = maxWidth * 0.6;
+          final double minHeight = 50;
 
-       return SingleChildScrollView(
+          return SingleChildScrollView(
             child: Form(
               key: _formKey,
               child:Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: constraints.maxHeight * 0.07),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Log In',
-                        style: TextStyle(
-                          fontSize: constraints.maxWidth * 0.07,
-                          color: mainColor,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: constraints.maxHeight * 0.09),
-                  Container(
-                    width: fieldWidth,
-                    child: EditBox(text: "Email",hintText: "upXXXXXXXXX@up.pt"),
-                  ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(
-                    width: fieldWidth,
-                    child: EditBox(
-                      text: "Password",
-                      hintText: "Password",
-                    ),
-                  ),
-                  SizedBox(height: constraints.maxHeight * 0.05),
-                  Container(
-                    height: constraints.maxHeight * 0.08,
-                    margin: EdgeInsets.only(top: constraints.maxHeight * 0.025, bottom: constraints.maxHeight * 0.0125),
-                    width: constraints.maxWidth * 0.5,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => SearchPage()
-                            )
-                          );
-                        }
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(mainColor),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25.0),
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: constraints.maxHeight * 0.07),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Log In',
+                          style: TextStyle(
+                            fontSize: constraints.maxWidth * 0.07,
+                            color: mainColor,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                      child: Text(
-                        "Enter",
-                        style: TextStyle(
-                          fontSize: constraints.maxWidth * 0.045,
-                          fontWeight: FontWeight.bold,
+                    ),
+                    SizedBox(height: constraints.maxHeight * 0.09),
+                    Container(
+                      width: fieldWidth,
+                      child: EditBox(text: "Email",hintText: "upXXXXXXXXX@up.pt"),
+                    ),
+                    SizedBox(height: constraints.maxHeight * 0.0025),
+                    Container(
+                      width: fieldWidth,
+                      child: EditBox(
+                        text: "Password",
+                        hintText: "Password",
+                      ),
+                    ),
+                    SizedBox(height: constraints.maxHeight * 0.05),
+                    Container(
+                      height: constraints.maxHeight * 0.08,
+                      margin: EdgeInsets.only(top: constraints.maxHeight * 0.025, bottom: constraints.maxHeight * 0.0125),
+                      width: constraints.maxWidth * 0.5,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => SearchPage()
+                            )
+                            );
+                          }
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(mainColor),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          "Enter",
+                          style: TextStyle(
+                            fontSize: constraints.maxWidth * 0.045,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: constraints.maxHeight * 0.025),
-                  //Spacer(),
-                  Container(
-  height: constraints.maxHeight * 0.03,
-  width: constraints.maxWidth * 0.75,
-  child: ElevatedButton(
-    onPressed: () {
-      Navigator.of(context).push(MaterialPageRoute(builder: (context) => signUpPage()));
-    },
-    style: ButtonStyle(
-      backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-      elevation: MaterialStateProperty.all<double>(0),
-      side: MaterialStateProperty.all<BorderSide>(BorderSide.none),
-      overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
-    ),
-    child: FittedBox(
-      fit: BoxFit.scaleDown,
-      alignment: Alignment.center,
-      child: Text(
-        "Don't have an account?",
-        style: TextStyle(
-          fontSize: constraints.maxWidth * 0.045,
-          fontWeight: FontWeight.bold,
-          color: mainColor,
-        ),
-      ),
-    ),
-  ),
-),
-                ],
+                    SizedBox(height: constraints.maxHeight * 0.025),
+                    //Spacer(),
+                    Container(
+                      height: constraints.maxHeight * 0.03,
+                      width: constraints.maxWidth * 0.75,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignUpPage()));
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                          elevation: MaterialStateProperty.all<double>(0),
+                          side: MaterialStateProperty.all<BorderSide>(BorderSide.none),
+                          overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                        ),
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Don't have an account?",
+                            style: TextStyle(
+                              fontSize: constraints.maxWidth * 0.045,
+                              fontWeight: FontWeight.bold,
+                              color: mainColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      },
-    ),
-  );
-}
+          );
+        },
+      ),
+    );
+  }
 }
 
 class EditBox extends StatefulWidget {
--- app/lib/authentication/signUp.dart
+++ app/lib/authentication/signUp.dart
@@ -3,161 +3,168 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../posts/timeline.dart';
 import 'dart:ui';
+import '../terms&guidelines/privacyAcceptance.dart';
 import 'login.dart';
 
 
-class signUpPage extends StatefulWidget {
+class SignUpPage extends StatefulWidget {
   @override
-  State<signUpPage> createState() => _signUpPageState();
+  State<SignUpPage> createState() => _signUpPageState();
 }
 
-class _signUpPageState extends State<signUpPage>{
+class _signUpPageState extends State<SignUpPage>{
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool _passwordVisible = false;
-  
- Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      leading: GoBackButton(),
-      backgroundColor: mainColor,
-      toolbarHeight: 80,
-    ),
-       body: LayoutBuilder(
-      builder: ( context, constraints) {
-        final double maxWidth = constraints.maxWidth;
-        final double fieldWidth = maxWidth * 1;
-        final double buttonWidth = maxWidth * 0.6;
-        final double minHeight = 50;
 
-        return SingleChildScrollView(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: Key("Sign Page"),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        backgroundColor: mainColor,
+        toolbarHeight: 80,
+      ),
+      body: LayoutBuilder(
+        builder: ( context, constraints) {
+          final double maxWidth = constraints.maxWidth;
+          final double fieldWidth = maxWidth * 1;
+          final double buttonWidth = maxWidth * 0.6;
+          final double minHeight = 50;
+
+          return SingleChildScrollView(
             child: Form(
               key: _formKey,
               child:Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0), 
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: constraints.maxHeight * 0.07),
-                  Container(
-                    margin: EdgeInsets.only(bottom:10),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          fontSize: constraints.maxWidth * 0.07,
-                          color: mainColor,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(
-                    width: fieldWidth,
-                    child: EditBox(text: "Name", hintText: "Name", isPassword: false)
-                    ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(
-                    width: fieldWidth,
-                    child: EditBox(text: "Email", hintText: "upXXXXXXXXX@up.pt", isPassword: false)
-                    ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(
-                    width: fieldWidth,
-                    child: EditBox(text: "Phone Number", hintText: "Phone Number", isPassword: false)
-                    ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(
-                    width: fieldWidth,
-                    child: EditBox(text: "Password", hintText: "Enter password", isPassword: true)
-                    ),
-                  SizedBox(height: constraints.maxHeight * 0.0025),
-                  Container(                   
-                    width: fieldWidth,
-                    child: EditBox(text: "Confirm Password", hintText: "Confirm password", isPassword: true)
-                    ),
-
-                  SizedBox(height: constraints.maxHeight * 0.05),
-                  Container(
-                    height: constraints.maxHeight * 0.08,
-                    margin: EdgeInsets.only(top: constraints.maxHeight * 0.025, bottom: constraints.maxHeight * 0.0125),
-                    width: constraints.maxWidth * 0.5,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => ProfilePage()
-                            )
-                          );
-                        }
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(mainColor),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25.0),
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: constraints.maxHeight * 0.07),
+                    Container(
+                      margin: EdgeInsets.only(bottom:10),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontSize: constraints.maxWidth * 0.07,
+                            color: mainColor,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                      child: Text(
-                        "Register",
-                        style: TextStyle(
-                          fontSize: constraints.maxWidth * 0.045,
-                          fontWeight: FontWeight.bold,
+                    ),
+                    SizedBox(height: constraints.maxHeight * 0.0025),
+                    Container(
+                        width: fieldWidth,
+                        child: EditBox(key: Key("Name"),text: "Name", hintText: "Name", isPassword: false)
+                    ),
+                    SizedBox(height: constraints.maxHeight * 0.0025),
+                    Container(
+                        width: fieldWidth,
+                        child: EditBox(key: Key("Email"),text: "Email", hintText: "upXXXXXXXXX@up.pt", isPassword: false)
+                    ),
+                    SizedBox(height: constraints.maxHeight * 0.0025),
+                    Container(
+                        width: fieldWidth,
+                        child: EditBox(key: Key("Phone Number"),text: "Phone Number", hintText: "Phone Number", isPassword: false)
+                    ),
+                    SizedBox(height: constraints.maxHeight * 0.0025),
+                    Container(
+                        width: fieldWidth,
+                        child: EditBox(key: Key("Password"),text: "Password", hintText: "Enter password", isPassword: true)
+                    ),
+                    SizedBox(height: constraints.maxHeight * 0.0025),
+                    Container(
+                        width: fieldWidth,
+                        child: EditBox(key: Key("Password Confirm"),text: "Confirm Password", hintText: "Confirm password", isPassword: true)
+                    ),
+
+                    SizedBox(height: constraints.maxHeight * 0.05),
+                    Container(
+                      key: Key("Register"),
+                      height: constraints.maxHeight * 0.08,
+                      margin: EdgeInsets.only(top: constraints.maxHeight * 0.025, bottom: constraints.maxHeight * 0.0125),
+                      width: constraints.maxWidth * 0.5,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => PrivacyAcceptancePage()
+                            )
+                            );
+                          }
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(mainColor),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          "Register",
+                          style: TextStyle(
+                            fontSize: constraints.maxWidth * 0.045,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: constraints.maxHeight * 0.025),
-                  Container(
-  height: constraints.maxHeight * 0.03,
-  width: constraints.maxWidth * 0.75,
-  child: ElevatedButton(
-    onPressed: () {
-      Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
-    },
-    style: ButtonStyle(
-      backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-      elevation: MaterialStateProperty.all<double>(0),
-      side: MaterialStateProperty.all<BorderSide>(BorderSide.none),
-      overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
-    ),
-    child: FittedBox(
-      fit: BoxFit.scaleDown,
-      alignment: Alignment.center,
-      child: Text(
-        "Already have an account?",
-        style: TextStyle(
-          fontSize: constraints.maxWidth * 0.045,
-          fontWeight: FontWeight.bold,
-          color: mainColor,
-        ),
+                    SizedBox(height: constraints.maxHeight * 0.025),
+                    Container(
+                      height: constraints.maxHeight * 0.03,
+                      width: constraints.maxWidth * 0.75,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                          elevation: MaterialStateProperty.all<double>(0),
+                          side: MaterialStateProperty.all<BorderSide>(BorderSide.none),
+                          overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                        ),
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Already have an account?",
+                            style: TextStyle(
+                              fontSize: constraints.maxWidth * 0.045,
+                              fontWeight: FontWeight.bold,
+                              color: mainColor,
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-          ),
-      
-        );
-      },
-    ),
-  );
-}
+            ),
+
+          );
+        },
+      ),
+    );
+  }
 }
 
 
 class EditBox extends StatefulWidget {
+  final Key key;
   final String text;
   final String hintText;
   final bool isPassword;
   final TextEditingController? controller;
 
-  EditBox({required this.text, required this.hintText, this.isPassword = false, this.controller});
+  EditBox({required this.text, required this.hintText, this.isPassword = false, this.controller, required this.key});
 
   @override
   _EditBoxState createState() => _EditBoxState();

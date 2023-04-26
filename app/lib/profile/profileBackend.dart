186905ef59461f2f2b1e05462e8ba13c0b20335c
guu1gas <96118509+guu1gas@users.noreply.github.com>
2023-04-26 22:40:30 +0100

    Add files via upload

--- app/lib/profile/profileBackend.dart
+++ app/lib/profile/profileBackend.dart
@@ -1,4 +1,5 @@
-import 'dart:io';
+ import 'dart:io';
+import 'userModel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -22,4 +23,10 @@ abstract class profileBackend{
     DocumentSnapshot snapshot = await firestore.collection("Users").doc(user!.email).get();
     return snapshot.data() as Map<String, dynamic>;
   }
+
+  Future<void> updateUserData(UserModel user) async {
+    await firestore.collection("Users").doc(user.email).update(user.toJson());
+  }
 }
+
+

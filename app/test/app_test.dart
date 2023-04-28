2f1d65f7ba618f3357496b0f0a69bf59bc7ce959
Matilde Simões <up202108782@up.pt>
2023-04-28 15:41:00 +0100

    one more fix

--- app/test/app_test.dart
+++ app/test/app_test.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
-
 import 'package:glob/glob.dart';
-
 import 'steps/initial_tests.dart';
 import 'steps/signUp.dart';
 

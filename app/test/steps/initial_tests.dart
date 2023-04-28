97b89fdc29fef1ad5bdafa16e52b5ab79c0c7aaf
Matilde Simões <up202108782@up.pt>
2023-04-28 15:36:49 +0100

    fix

--- app/test/steps/initial_tests.dart
+++ app/test/steps/initial_tests.dart
@@ -2,8 +2,6 @@ import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
 import 'package:flutter_driver/flutter_driver.dart';
 
-
-
 class UserIsInInitialPage extends GivenWithWorld<FlutterWorld> {
   @override
   Future<void> executeStep() async {

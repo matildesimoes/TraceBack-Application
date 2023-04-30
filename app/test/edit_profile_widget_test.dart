9c11f0758a4755102619aa3fcf5271c26a0a90bb
Ramos <1joaoramos1@gmail.com>
2023-04-30 16:27:22 +0100

    Fixed Mock Constructor

--- app/test/edit_profile_widget_test.dart
+++ app/test/edit_profile_widget_test.dart
@@ -4,7 +4,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:TraceBack/profile/editprofile.dart';
 
 
-class  EditProfilePageMock extends EditProfilePage  {
+class EditProfilePageMock extends EditProfilePage  {
+
+  EditProfilePageMock(super.refresh);
+
   @override
   State<EditProfilePage> createState() => _MockEditProfilePageState();
 }
@@ -35,7 +38,7 @@ void main() {
   
   testWidgets('Find first EditBox widget', (WidgetTester tester) async {
 
-    await tester.pumpWidget(MaterialApp(home: EditProfilePageMock()));
+    await tester.pumpWidget(MaterialApp(home: EditProfilePageMock((){})));
 
 
     final editBoxName = find.byType(EditBox);

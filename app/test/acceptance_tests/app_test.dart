a54f97361f2cc54d030344d65dae71fb0b860872
Ramos <1joaoramos1@gmail.com>
2023-05-02 10:30:28 +0100

    fixing ATs

--- app/test/acceptance_tests/app_test.dart
+++ app/test/acceptance_tests/app_test.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:gherkin/gherkin.dart';
 
 import 'package:glob/glob.dart';
